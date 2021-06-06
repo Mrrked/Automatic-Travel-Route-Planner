e5eef6703db4bb58446f40f7371e88ed11459c1b
MrYious <rosariomark37@gmail.com>
2021-06-06 11:41:29 +0800

    v1.4
    Minor Changes:
    Navbar turned into Drawer.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,76 +1,197 @@
+/*
+  TO DO:
+      1.  
+*/
+//Standard Imports
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+//GoogleMap Imports
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
-import { makeStyles } from '@material-ui/core/styles';
+
+//Material UI Imports
+import clsx from 'clsx';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 
+//Global Elements
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
+    display: 'flex',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
   },
   menuButton: {
     marginRight: theme.spacing(2),
   },
-})); //Material CSS Appbar
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(0),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
+  },
+}));  //Material UI
+
+const drawerWidth = 240;  //Drawer
 
 const containerStyle = {
-  float: 'right',
-  width: '1000px',
-  height: '615px',
-  margin: '0px'
-};
+  float: 'center',
+  width: '100%',
+  height: '599px',
+  margin: '0'
+}; //Google Map Size and Orientation
 
 const center = {
   lat: 14.599513,
   lng: 120.984222
-};
+}; // Google Map Initial Location
 
+
+//Render Contents
 function MyComponent() {
-  const classes = useStyles(); //Material CSS
+  const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
 
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  }; //Material UI
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  }; //Material UI
+  
+  //Check Google Map API Connection
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: "AIzaSyDr-Sb2ICpI-9HRANMJBLIOEAzAHNNWjbk" //AIzaSyDPCx-DR57YVb-1pYfEwi9EsvWUqLWMKmA
-  })                                                            //AIzaSyDr-Sb2ICpI-9HRANMJBLIOEAzAHNNWjbk
+  }) //Map                                                           //AIzaSyDr-Sb2ICpI-9HRANMJBLIOEAzAHNNWjbk
 
-  const [map, setMap] = React.useState(null)
+  const [map, setMap] = React.useState(null)  //Map
 
   const onLoad = React.useCallback(function callback(map) {
     const bounds = new window.google.maps.LatLngBounds();
     map.fitBounds(bounds);
     setMap(map)
-  }, [])
+  }, [])  //Map
 
   const onUnmount = React.useCallback(function callback(map) {
     setMap(null)
-  }, [])
+  }, [])  //Map
+
   return (
     <div className={classes.root}>
-      <AppBar position="sticky">
-        <Toolbar variant="dense">
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+      <CssBaseline />
+      <AppBar
+        position="fixed"
+        className={clsx(classes.appBar, {
+          [classes.appBarShift]: open,
+        })}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            className={clsx(classes.menuButton, open && classes.hide)}
+          >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" color="inherit">
+          <Typography variant="h6" noWrap>
             Travel Route Planner
           </Typography>
         </Toolbar>
       </AppBar>
-    
-    {isLoaded && <GoogleMap
-      mapContainerStyle={containerStyle}
-      center={center}
-      zoom={11}
-      onLoad={onLoad}
-      onUnmount={onUnmount}
-    >
-      { /* Child components, such as markers, info windows, etc. */ }
-      <></>
-    </GoogleMap>}
+      <Drawer
+        className={classes.drawer}
+        variant="persistent"
+        anchor="left"
+        open={open}
+        classes={{
+          paper: classes.drawerPaper,
+        }}
+      >
+        <div className={classes.drawerHeader}>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </div>
+        <Divider />
+        
+        <List>         
+          
+          Panel
+        </List>
+      </Drawer>
+      <main className={clsx(classes.content, {[classes.contentShift]: open,})}>
+      <div className={classes.drawerHeader} />        
+        {isLoaded && 
+          <GoogleMap                                                              //Interactive Map
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={11}
+          onLoad={onLoad}
+          onUnmount={onUnmount}>
+          { /* Child components, such as markers, info windows, etc. */ }
+          <></>
+        </GoogleMap>}
+        </main>
     </div>
   );
 
@@ -79,4 +200,5 @@ function MyComponent() {
     
 }
 
+//Render
 export default React.memo(MyComponent)
