af8f4751caeaf08f2501033a526895549603da0a
Eigel <eigelasinas1962@gmail.com>
2021-06-09 15:39:51 +0800

    change display

--- my-app/src/components/Panel.js
+++ my-app/src/components/Panel.js
@@ -2,13 +2,15 @@ import { v4 as uuid } from "uuid"
 import {
     Box,
     Button,
+    Fab,
     IconButton,
     Paper,
     Typography,
   } from "@material-ui/core"
 import {
-    Add as AddIcon,
+    Add as AddLocationIcon,
     Clear as ClearIcon,
+    SearchOutlined as FindRouteIcon
 } from "@material-ui/icons"
 import AutocompleteField from "components/AutocompleteField";
 import { getValidLocations } from "utils"
@@ -16,22 +18,30 @@ import { getValidLocations } from "utils"
 export default function Panel({ locations, setLocations, setRoute, handleGenerateRoute }){
     const validLocations = getValidLocations(locations)
 
-    return <Box width={300} display="flex" flexDirection="column" alignSelf="stretch" component={Paper}>
-        <Box p={3} width="100%" color="primary.contrastText" bgcolor="primary.main">
+    return <Box zIndex={1} boxShadow={2} width={300} display="flex" flexDirection="column" alignSelf="stretch" component={Paper}>
+        <Box boxShadow={1} p={3} color="primary.contrastText" bgcolor="primary.main">
             <Typography variant="h6" color="inherit">
                 Travel Route Planner
             </Typography>
         </Box>
-        <Box p={3} flexGrow={1} flexShrink={1} overflow="scroll" display="flex" flexDirection="column">
-            <Box overflow="scroll">
-                {locations.map((location, index) => 
-                <Box
-                    key={location.id}
-                    mb={1}
-                    display="flex"
-                    alignItems="center"
-                    justifyContent="space-between"
+        <Box px={3} pt={1} flexGrow={1} flexShrink={1} overflow="scroll" display="flex" flexDirection="column">
+            <Box mb={2} display="flex" justifyContent="flex-end">
+                <Button
+                    disabled={locations.some(loc => !loc.value)}
+                    startIcon={<AddLocationIcon />}
+                    onClick={_ => setLocations(old => [...old, { id: uuid() }])}
                 >
+                    Add Location
+                </Button>
+            </Box>
+            {locations.map((location, index) => 
+            <Box
+                key={location.id}
+                mb={1}
+                display="flex"
+                alignItems="center"
+                justifyContent="space-between"
+            >
                 <Box width={220} mr={1}>
                     <AutocompleteField
                     label={index === 0 ? "Start" : `Destination ${index}`}
@@ -52,27 +62,19 @@ export default function Panel({ locations, setLocations, setRoute, handleGenerat
                     >
                         <ClearIcon fontSize="small"/>
                     </IconButton>}
-                </Box>
-                )}
             </Box>
-            <Box mb={2}>
-                <Button
-                    disabled={locations.some(loc => !loc.value)}
-                    startIcon={<AddIcon />}
-                    onClick={_ => setLocations(old => [...old, { id: uuid() }])}
-                >
-                    Add Location
-                </Button>
-            </Box>
-            <Box mb={2}>
-                <Button
-                    color="primary"
-                    disabled={validLocations.length < 2}
-                    onClick={handleGenerateRoute}
-                >
-                    Generate Route
-                </Button>
-            </Box>
+            )}
+        </Box>
+        <Box p={3} display="flex" justifyContent="flex-end">
+            <Fab 
+                variant="extended" 
+                color="primary"
+                disabled={validLocations.length < 2}
+                onClick={handleGenerateRoute}
+            >
+                <FindRouteIcon style={{ marginRight: 10 }} />
+                Find Route
+            </Fab>
         </Box>
     </Box>
 }
